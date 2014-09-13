ca84647abc1d41e7e6d85c5077d274a461164589
Rahul Mohandas <rahul@rahulmohandas.com>
2014-09-13 15:40:26 -0400

    updated

--- src/warmup/utopian_tree.py
+++ src/warmup/utopian_tree.py
@@ -1,21 +1,14 @@
-class Tree:
-    def __init__(self):
-        self.__height = 0
-
-    def grow(self, num_cycles = 0):
-        for i in range(0, num_cycles+1):
-            if i % 2 == 0:
-                self.__height += 1
-            else:
-                self.__height += self.__height
-
-    def get_height(self):
-        print self.__height
+def tree_height(num_cycles = 0):
+    height = 0
+    for i in range(0, num_cycles+1):
+        if i % 2:
+            height += height
+        else:
+            height += 1
+    return height
         
 if __name__ == '__main__':
     num_tests = input()
     for i in range(0, num_tests):
-        t = Tree()
-        num_cycles = input()
-        t.grow(num_cycles)
-        t.get_height()
+        n = input()
+        print tree_height(n)
